09e51259fda7cc5c6a813648ed5a32c92730316c
Allan Bifom (aeb672) <aeb672@usask.ca>
2023-05-11 18:05:17 -0600

    finished beverage app framework

--- beverage/models.py
+++ beverage/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class beverage(models.Model):
+
+
+class Beverage(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     quantity = models.IntegerField()
--- beverage/views.py
+++ beverage/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Beverage
+
 
 def home(request):
-    return HttpResponse('<h1>Home Page </h1>')
+    beverage = Beverage.objects.all()
+    return render(request, 'homePage.html', {'beverage': beverage})
